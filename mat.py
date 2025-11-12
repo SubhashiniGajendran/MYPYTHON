c019aaeb8c1ae217cb0c29dd056e2223d9c0357d
SubhashiniGajendran <subhashini.gajendran@gmail.com>
2025-11-12 21:59:04 +0530

    Add bar chart for marks by subject

--- mat.py
+++ mat.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 subjects = ['Math', 'Science', 'English', 'History']
 marks = [80, 90, 70, 85]
-
+# CHANGED
 plt.bar(subjects, marks, color='skyblue')
 plt.title("Marks by Subject")
 plt.xlabel("Sub")
